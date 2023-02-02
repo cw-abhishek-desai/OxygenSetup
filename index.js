ed2fcf2aa2b8a6a523c322296384089e54bc12ac
Abhishek Desai <abhishek.d@carwale.com>
2023-02-02 16:16:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './src/App.js'
+
+const container = document.getElementById('root');
+ReactDOM.render(<App />, container);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@oxygen/core';
+
+const App = () =>{
+    return (
+        <h1>
+            Hello world! I am using React
+            I s it getting updated
+        <Button>Hello is Oxygen Working</Button>
+
+        </h1>
+    )
+}
+
+export default App;
